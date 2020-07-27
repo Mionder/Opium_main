56d9b27001b908e3e942baf6700be076bd88a921
Mionder <49869397+Mionder@users.noreply.github.com>
2020-07-27 16:31:59 +0300

    Error and Blog pages

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import MainPage from "./Components/MainPage/MainPage";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Error404 from "./Components/Error404/Error404";
+import BlogPage from "./Components/BlogPage/BlogPage";
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Route  path="/" component={MainPage} exact/>
         <Route  path="/error" component={Error404} exact/>
+        <Route path="/blog" component={BlogPage} exact />
       </div>
     </Router>
   );
